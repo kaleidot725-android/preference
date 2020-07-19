45ab1a61fd175afada2b6eb8120115133d74ceb7
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-07-19 23:50:33 +0900

    Add EditTextPreference

--- src/app/src/main/java/jp/kaleidot725/sample/setting/ParentPreferenceFragment.kt
+++ src/app/src/main/java/jp/kaleidot725/sample/setting/ParentPreferenceFragment.kt
@@ -1,11 +1,18 @@
 package jp.kaleidot725.sample.setting
 
 import android.os.Bundle
+import androidx.preference.EditTextPreference
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import jp.kaleidot725.sample.R
 
 class ParentPreferenceFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.parent_preferences, rootKey)
+
+        val editTextPreference = findPreference<EditTextPreference>("editTextPreference")
+        editTextPreference?.summaryProvider = Preference.SummaryProvider<EditTextPreference> {
+            "value :" + it.text
+        }
     }
 }
