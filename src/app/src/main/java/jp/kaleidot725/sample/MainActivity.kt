7a1ca4cb9960eb57f0dca336e712b8e6d3e3ac68
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-07-19 23:50:13 +0900

    Refactor MainActivity

--- src/app/src/main/java/jp/kaleidot725/sample/MainActivity.kt
+++ src/app/src/main/java/jp/kaleidot725/sample/MainActivity.kt
@@ -6,15 +6,11 @@ import androidx.preference.PreferenceFragmentCompat
 
 class MainActivity : AppCompatActivity(R.layout.activity_main), PreferenceFragmentCompat.OnPreferenceStartFragmentCallback{
     override fun onPreferenceStartFragment(caller: PreferenceFragmentCompat, pref: Preference): Boolean {
-        val args = pref.extras
-        val fragment = supportFragmentManager.fragmentFactory.instantiate(classLoader, pref.fragment)
-        fragment.arguments = args
-        fragment.setTargetFragment(caller, 0)
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.fragment_container, fragment)
-            .addToBackStack(null)
-            .commit()
+        val fragment = supportFragmentManager.fragmentFactory.instantiate(classLoader, pref.fragment).apply {
+            arguments = pref.extras
+            setTargetFragment(caller, 0)
+        }
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment).addToBackStack(null).commit()
         return true
     }
 }
