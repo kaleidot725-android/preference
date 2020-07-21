af9f980b90b0bc58071c2699b075c2bdc7e0b1bf
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-07-21 09:01:48 +0900

    update

--- src/app/src/main/java/jp/kaleidot725/sample/setting/ParentPreferenceFragment.kt
+++ src/app/src/main/java/jp/kaleidot725/sample/setting/ParentPreferenceFragment.kt
@@ -1,14 +1,29 @@
 package jp.kaleidot725.sample.setting
 
 import android.os.Bundle
-import androidx.preference.EditTextPreference
-import androidx.preference.Preference
-import androidx.preference.PreferenceFragmentCompat
+import android.widget.SeekBar
+import androidx.preference.*
 import jp.kaleidot725.sample.R
 
 class ParentPreferenceFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.parent_preferences, rootKey)
+
+
+        val dropDownPreference = findPreference<DropDownPreference>("dropDownPreference")
+        dropDownPreference?.summaryProvider = Preference.SummaryProvider<DropDownPreference> {
+            "entry :" + it.entry + " value : " + it.value
+        }
+
+        val listPreference = findPreference<ListPreference>("listPreference")
+        listPreference?.summaryProvider = Preference.SummaryProvider<ListPreference> {
+            "entry :" + it.entry + " value :" + it.value
+        }
+
+        val multiSelectListPreference = findPreference<MultiSelectListPreference>("multiSelectListPreference")
+        multiSelectListPreference?.summaryProvider = Preference.SummaryProvider<MultiSelectListPreference> {
+             "values :" + it.values
+        }
 
         val editTextPreference = findPreference<EditTextPreference>("editTextPreference")
         editTextPreference?.summaryProvider = Preference.SummaryProvider<EditTextPreference> {
